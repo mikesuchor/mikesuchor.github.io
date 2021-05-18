e2f3f10c01ea88b211e00be94705807f991ae2fb
mikesuchor <mikesuchor@gmail.com>
2021-05-17 21:54:38 -0400

    Updated photos and minor adjustments

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,10 +2,9 @@ $(document).ready(() => {
   centerHeroText();
 
   const aboutMosaic = $('.about__mosaic');
-  const projectsImages = $('.project__right__half');
+  const projectsImages = $('.project__right__column');
 
   const aboutMosaicObserver = new IntersectionObserver((entry) => {
-    console.log(entry[0].intersectionRatio);
     if (entry[0].intersectionRatio > 0) {
       aboutMosaic.children().addClass('animated');
     } else {
